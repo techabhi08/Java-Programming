82a1a24383493c77d830c445e0ddd453117a3884
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2021-10-08 15:52:04 +0530

    Add files via upload

---
+++ Scope in Java/Scope/src/com/abhinaygarg/Main.java
@@ -0,0 +1,18 @@
+package com.abhinaygarg;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String varFour = "this is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        scopeInstance.useInner();
+        System.out.println("scopeInstance varOne is: " + scopeInstance.getVarOne());
+        System.out.println(varFour);
+
+        scopeInstance.timesTwo();
+
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        innerClass.timesTwo();
+    }
+}
